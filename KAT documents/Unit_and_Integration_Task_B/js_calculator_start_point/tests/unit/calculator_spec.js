caf2a3fa1d75ece20cdda2443bbb9864c2b933c8
Andrew Samtoy <asamtoy@gmail.com>
2017-12-15 16:56:17 +0000

    some tests passing

--- KAT documents/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ KAT documents/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,63 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  //  - calculator.add()
+  it('it can add', function(){
+    calculator.previousTotal = 3
+    calculator.add(5)
+    assert.equal(calculator.runningTotal, 8)
+  })
+
+  // - calculator.subtract()
+  it('it can subtract', function(){
+    calculator.previousTotal = 100
+    calculator.subtract(5)
+    assert.equal(calculator.runningTotal, 95)
+  })
+
+  // - calculator.multiply()
+  it('it can multiply', function(){
+    calculator.previousTotal = 5
+    calculator.multiply(5)
+    assert.equal(calculator.runningTotal, 25)
+  })
+
+  // - calculator.divide()
+  it('it can divide', function(){
+    calculator.previousTotal = 9
+    calculator.divide(3)
+    assert.equal(calculator.runningTotal, 3)
+  })
+
+  // - calculator.numberClick()
+  it('it can register a numberclick', function(){
+    calculator.numberClick(5)
+    assert.equal(calculator.runningTotal, 5)
+  })
+
+  // - calculator.operatorClick()
+  it('it can register an operator click', function() {
+
+      calculator.numberClick(7)
+      calculator.operatorClick("-")
+      calculator.numberClick(4)
+      calculator.operatorClick("=")
+
+      assert.equal(3, calculator.runningTotal)
+    });
+
+  // - calculator.clearClick()
+  it('it can register a clear click', function() {
+
+      calculator.numberClick(7)
+      calculator.operatorClick("-")
+      calculator.numberClick(4)
+      calculator.clearClick()
+      calculator.numberClick(9)
+      calculator.operatorClick("=")
+
+      assert.equal(-2, calculator.runningTotal)
+    });
+
+
 });
